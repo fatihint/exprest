3cfb7bbc9978c185cc9f21c7858ab5dd911f45ce
fatihint <fatihint@gmail.com>
2018-10-17 18:54:14 +0300

    POST /posts test updated

--- tests/post.test.js
+++ tests/post.test.js
@@ -22,13 +22,16 @@ const posts = [
 ]
 
 beforeEach(function () {
+    Post.remove({}, function(err) {
+        if (err) throw err
+    })
     posts.forEach(post => {
         var postDoc = new Post(post)
         postDoc.save().then().catch()
     })
 })
 
-afterEach(function() {
+afterEach(function () {
     posts.forEach(post => {
         Post.findOneAndRemove({
             _id: post._id
@@ -98,56 +101,54 @@ describe('POST', () => {
     })
 
     describe('POST /posts', () => {
-        
+
         var post = {
             title: 'titltltltl',
             body: 'bobobybyb'
         }
 
-        after(function() {
-            Post.findOneAndRemove({
-                title: post.title
-            })
-        })
-
         it('should save a new post and return it', (done) => {
             request(app)
                 .post('/posts')
                 .send(post)
-                .set('Accept', 'application/json')
                 .expect(200)
                 .expect((res) => {
                     expect(res.body._id).to.not.be.empty()
                     expect(res.body.title).to.equal(post.title)
                     expect(res.body.body).to.equal(post.body)
                 })
+                .end((err, result) => {
+                    if (err) {
+                        return done(err)
+                    }
+                    done()
+                })
+        })
+
+        it('should return error with invalid data', (done) => {
+            request(app)
+                .post('/posts')
+                .send({ title: 'b' })
+                .expect(400)
                 .end((err, res) => {
                     if (err) {
                         return done(err)
                     }
                     done()
                 })
-
-            // Post.findOne({
-            //     title: post.title
-            // })
-            //     .then((result) => {
-            //         // expect(result.title.to.equal(post.title))
-            //         expect(result.body.to.equal(post.body))
-            //         done()
-            //     })
-            //     .catch(err => done(err))
-
         })
 
-        it('should return error with invalid data', (done) => {
+        it('shouldn\'t save post if title exist', (done) => {
             request(app)
                 .post('/posts')
-                .send({title: 'b'})
+                .send({title: posts[0].title, body: posts[0].body})
                 .expect(400)
+                .expect((res) => {
+                    expect(res.body.errmsg).to.not.be.empty()
+                })
                 .end((err, res) => {
                     if (err) {
-                        return done(err)
+                        done(err)
                     }
                     done()
                 })
