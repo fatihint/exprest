9cc9e9897e50cf7790ed98adfe6ffb03ff4b38e3
fatihint <fatihint@gmail.com>
2018-10-18 10:29:29 +0300

    Password is hashed before saving user

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('../db/mongoose')
 const _ = require('lodash')
 const validator = require('validator')
+const bcrypt = require('bcrypt')
 
 var UserSchema = new mongoose.Schema({
     email: {
@@ -32,6 +33,21 @@ UserSchema.methods.toJSON = function() {
     var userObj = this.toObject()
     return _.pick(userObj, ["_id", "email", "createdAt", "updatedAt"])
 }
+
+UserSchema.pre('save', function(next) {
+    const user = this
+    
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash
+                next()
+            }) 
+        })
+    } else {
+        next()
+    }
+})
 
 var User = mongoose.model('User', UserSchema)
 
