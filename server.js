a9da18689fcb99cb9e3c8834e2ed9c433411eb87
fatihint <fatihint@gmail.com>
2018-10-23 16:28:17 +0300

    PORT env configured.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const postRouter = require('./routes/post')
 const userRouter = require('./routes/user')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 
